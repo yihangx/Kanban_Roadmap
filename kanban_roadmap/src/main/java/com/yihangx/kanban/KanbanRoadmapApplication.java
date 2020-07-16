7e452cf18a8f9e3bc181d7b2881785398d4fbf97
Yihang Xin <yx141@duke.edu>
2020-07-15 21:28:47 -0400

    start

---
+++ kanban_roadmap/src/main/java/com/yihangx/kanban/KanbanRoadmapApplication.java
@@ -0,0 +1,13 @@
+package com.yihangx.kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanRoadmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanRoadmapApplication.class, args);
+	}
+
+}
---
+++ kanban_roadmap/src/test/java/com/yihangx/kanban/KanbanRoadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yihangx.kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanRoadmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
